81c5b80b323c76060cfa4b4944e46941245ed73e
skoowu <skoowu.fancy@gmail.com>
2023-06-06 00:02:31 +0800

    update config

--- wezterm.lua
+++ wezterm.lua
@@ -1,67 +1,63 @@
 local wezterm = require("wezterm")
-local config = {
-	-- font
-	font = wezterm.font("VictorMono Nerd Font", { italic = false }),
-	font_size = 13,
 
-	-- shell
+return {
 	default_prog = { "zsh" },
-
-	-- window
-	window_background_opacity = 0.95,
-	window_padding = {
-		left = 5,
-		right = 5,
-		top = 5,
-		bottom = 5,
-	},
+	underline_position = -4,
+	underline_thickness = "200%",
+	force_reverse_video_cursor = true,
 	hide_tab_bar_if_only_one_tab = true,
 	hide_mouse_cursor_when_typing = true,
+	use_resize_increments = true,
 
-	-- window size
-	initial_cols = 210,
-	initial_rows = 50,
+	-- font
+	font = wezterm.font("VictorMono Nerd Font"),
+	font_size = 13,
 
 	-- colors
 	color_scheme = "hardhacker",
 
-	force_reverse_video_cursor = true,
+	-- window
+	window_background_opacity = 0.95,
+	window_padding = {
+		left = 2,
+		right = 2,
+		top = 1,
+		bottom = 0,
+	},
+	initial_cols = 210,
+	initial_rows = 50,
 
+	-- tab bar
 	window_frame = {
 		font_size = 12.0,
-		active_titlebar_bg = "#3f3951",
-		inactive_titlebar_bg = "#3f3951",
+		active_titlebar_bg = "#282433",
+		inactive_titlebar_bg = "#282433",
 	},
 	colors = {
 		tab_bar = {
 			background = "#282433",
-
 			active_tab = {
 				bg_color = "#282433",
 				fg_color = "#e965a5",
 
 				intensity = "Normal",
-				underline = "Single",
+				underline = "None",
 				italic = true,
 				strikethrough = false,
 			},
-
 			inactive_tab = {
-				bg_color = "#211e2a",
+				bg_color = "#282433",
 				fg_color = "#938aad",
 			},
-
 			inactive_tab_hover = {
 				bg_color = "#e192ef",
 				fg_color = "#eee9fc",
 				italic = true,
 			},
-
 			new_tab = {
 				bg_color = "#282433",
 				fg_color = "#938aad",
 			},
-
 			new_tab_hover = {
 				bg_color = "#e192ef",
 				fg_color = "#eee9fc",
@@ -70,5 +66,3 @@ local config = {
 		},
 	},
 }
-
-return config
