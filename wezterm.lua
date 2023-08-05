f3aadb4ffcb4e34b6c76716d72202c3bb6322786
skoowu <skoowu.fancy@gmail.com>
2023-08-05 13:08:50 +0800

    update padding

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,7 @@ wezterm.on("update-status", function(window, pane)
 		})
 	else
 		window:set_config_overrides({
-			window_padding = { left = 50, right = 50, top = 5, bottom = 30 },
+			window_padding = { left = 20, right = 20, top = 5, bottom = 10 },
 		})
 	end
 end)
