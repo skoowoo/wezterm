59b963a1ce8bb3f99881bf67eef1517a6513a090
Skoo Wu <skoowu.fancy@gmail.com>
2024-08-28 10:18:42 +0800

    update

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 
 --wezterm.on("update-status", function(window, pane)
 --	if pane:is_alt_screen_active() then
@@ -21,7 +22,7 @@ wezterm.on("update-status", function(window, pane)
 	end
 	if info.name == "nvim" then
 		window:set_config_overrides({
-			window_padding = { left = 1, right = 1, top = 15, bottom = 0 },
+			window_padding = { left = 1, right = 1, top = 5, bottom = 0 },
 		})
 	else
 		window:set_config_overrides({
@@ -30,6 +31,11 @@ wezterm.on("update-status", function(window, pane)
 			macos_window_background_blur = 20,
 		})
 	end
+end)
+
+wezterm.on("gui-startup", function()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
 end)
 
 return {
@@ -67,11 +73,26 @@ return {
 	},
 
 	-- colors
-	color_scheme = "hardhacker",
+	-- color_scheme = "hardhacker",
 	use_fancy_tab_bar = false,
 	window_decorations = "RESIZE | TITLE | MACOS_FORCE_ENABLE_SHADOW",
+	force_reverse_video_cursor = true,
+	colors = {
+		foreground = "#dcd7ba",
+		background = "#1f1f28",
 
-	-- window
-	initial_cols = 255,
-	initial_rows = 55,
+		cursor_bg = "#c8c093",
+		cursor_fg = "#c8c093",
+		cursor_border = "#c8c093",
+
+		selection_fg = "#c8c093",
+		selection_bg = "#2d4f67",
+
+		scrollbar_thumb = "#16161d",
+		split = "#16161d",
+
+		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+	},
 }
